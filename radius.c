d979bace3d90bf8b093ae0f9cfb7de07df2fb62d
Richard S. Graham <RichGraham@users.noreply.github.com>
2022-12-01 11:25:16 +0000

    Uses built-in sqrt function and other minor fixes

--- radius.c
+++ radius.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 double calculate_radius( double area);
 
@@ -6,21 +7,13 @@ int main()
 {
   double my_area = 2.0;
 
-  printf("A circle of area %f has a radiys of %f\n", my_area, calculate_radius(my_area) );
+  printf("A circle of area %f m^2 has a radiys of %f m\n", my_area, calculate_radius(my_area) );
 
 }
 
 double calculate_radius( double area)
 {
-  double pi = 3.14159;
-  double x, x_sq, x_cubed, x_4, x_5;
+  double pi = 3.14159265;
 
-  // Calculate the square root by a Taylor series in powers of x- 1 (a bad choice of method!)
-  x = area / pi -1.0;
-  x_sq = x*x;
-  x_cubed = x*x*x;
-  x_4 = x*x*x*x;
-  x_5 = x*x*x*x*x;
-
-  return 1.0 + 1.0/2.0*x - 1.0/8.0*x_sq + 1.0/16.0*x_cubed - 5.0/128.0*x_4 + 7.0/256.0*x_5;
+  return sqrt( area/pi);
 }
